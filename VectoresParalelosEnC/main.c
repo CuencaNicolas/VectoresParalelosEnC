daeba963af0dcef3728f19ecb928c58135318993
Cuenca Franco <36193890+CuencaNicolas@users.noreply.github.com>
2018-04-19 10:38:11 -0300

    Add files via upload

---
+++ VectoresParalelosEnC/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char nombre[50];
+    int notas[];
+    int notas[2];
+    float promedio;
+    printf("Sin vectores");
+    return 0;
+}
