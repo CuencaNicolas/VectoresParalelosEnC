591e8433c31a38ee463b1647f89ff15a67e57cce
Cuenca Franco <36193890+CuencaNicolas@users.noreply.github.com>
2018-04-19 11:07:48 -0300

    Add files via upload

--- VectoresParalelosEnC/main.c
+++ VectoresParalelosEnC/main.c
@@ -1,12 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define TAMANIO 5
+/*Vectores paralelos*/
 
+/*Ejemplos: */
+
+/*     | nombre          | edad
+--------------------------------
+1      | "Jose"          |  33
+2      | "Gabriel"       |  44
+3      |                 |
+4      |                 |
+5*     |                 |
+6      |                 |
+7      |                 |
+*/
 int main()
 {
-    char nombre[50];
-    int notasUno[];
-    int notasDos[];
-    float promedio;
+    char nombre[TAMANIO][50];
+    int notaUno[TAMANIO];
+    int notaDos[TAMANIO];
+    float promedio[TAMANIO];
+    int legajo[TAMANIO];
+
     printf("Sin vectores");
+
+
     return 0;
 }
